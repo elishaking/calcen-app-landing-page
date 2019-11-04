8b27ef97562fb2d5cc8f5050cb58dd7e774b04d1
King Elisha <ek.chibueze@gmail.com>
2019-11-04 01:36:47 +0100

    jsx

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -12,7 +12,7 @@ export default class Landing extends Component {
               <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48">
                 <g id="logo" transform="translate(-248 -123)">
                   <path id="Path_68" data-name="Path 68" d="M10,0H38A10,10,0,0,1,48,10V38A10,10,0,0,1,38,48H10A10,10,0,0,1,0,38V10A10,10,0,0,1,10,0Z" transform="translate(248 123)" fill="#005e8d" />
-                  <text id="r_Θ" data-name="r∠Θ" transform="translate(256 154)" fill="#fff" font-size="18" fontFamily="Rubik-Medium, Rubik" fontWeight="500"><tspan x="0" y="0">r</tspan><tspan y="0" fontFamily="SegoeUISymbol, Segoe UI Symbol" fontWeight="400">∠</tspan><tspan y="0" fontFamily="SegoeUI-Semibold, Segoe UI" fontWeight="600">Θ</tspan></text>
+                  <text id="r_Θ" data-name="r∠Θ" transform="translate(256 154)" fill="#fff" fontSize="18" fontFamily="Rubik-Medium, Rubik" fontWeight="500"><tspan x="0" y="0">r</tspan><tspan y="0" fontFamily="SegoeUISymbol, Segoe UI Symbol" fontWeight="400">∠</tspan><tspan y="0" fontFamily="SegoeUI-Semibold, Segoe UI" fontWeight="600">Θ</tspan></text>
                 </g>
               </svg>
 
