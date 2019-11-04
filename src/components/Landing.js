f8f4c2b67ea3029548bfef9d7264bdc019a877bb
King Elisha <ek.chibueze@gmail.com>
2019-11-04 01:33:35 +0100

    landing markup

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './Landing.scss';
+import GooglePlayIcon from './GooglePlayIcon';
 
 export default class Landing extends Component {
   render() {
@@ -11,7 +12,7 @@ export default class Landing extends Component {
               <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48">
                 <g id="logo" transform="translate(-248 -123)">
                   <path id="Path_68" data-name="Path 68" d="M10,0H38A10,10,0,0,1,48,10V38A10,10,0,0,1,38,48H10A10,10,0,0,1,0,38V10A10,10,0,0,1,10,0Z" transform="translate(248 123)" fill="#005e8d" />
-                  <text id="r_Θ" data-name="r∠Θ" transform="translate(256 154)" fill="#fff" font-size="18" font-family="Rubik-Medium, Rubik" font-weight="500"><tspan x="0" y="0">r</tspan><tspan y="0" font-family="SegoeUISymbol, Segoe UI Symbol" font-weight="400">∠</tspan><tspan y="0" font-family="SegoeUI-Semibold, Segoe UI" font-weight="600">Θ</tspan></text>
+                  <text id="r_Θ" data-name="r∠Θ" transform="translate(256 154)" fill="#fff" font-size="18" fontFamily="Rubik-Medium, Rubik" fontWeight="500"><tspan x="0" y="0">r</tspan><tspan y="0" fontFamily="SegoeUISymbol, Segoe UI Symbol" fontWeight="400">∠</tspan><tspan y="0" fontFamily="SegoeUI-Semibold, Segoe UI" fontWeight="600">Θ</tspan></text>
                 </g>
               </svg>
 
@@ -26,6 +27,18 @@ export default class Landing extends Component {
             </ul>
           </nav>
         </header>
+
+        <div className="content">
+          <div className="left">
+            <h1>Complex Arithmetic made Easy</h1>
+            <p>CalcEn is a complex number calculator that handles arithmetic of complex expressions, matrices and functions</p>
+            <GooglePlayIcon />
+          </div>
+
+          <div className="right">
+            <img src="../assets/img/phone.png" alt="Phone"/>
+          </div>
+        </div>
       </div>
     )
   }
